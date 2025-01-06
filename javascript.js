c2f8de2423c2e9b90ee145c0eb89638491290f12
Abhinav Karthik <abhinavkarthik.pattipati@gmail.com>
2025-01-06 14:02:48 +0530

    Complete Responsive Grid based on input

--- javascript.js
+++ javascript.js
@@ -9,7 +9,36 @@ for (let i = 1; i <= squaresPerRow * squaresPerRow; i++) {
   let divWidth = 640 / squaresPerRow;
   mydiv.style.width = `${divWidth}px`;
   mydiv.style.height = `${divWidth}px`;
-  mydiv.style.backgroundColor = "red";
-  mydiv.style.textAlign = "center";
   mydiv.textContent = `A`;
 }
+
+function updateGrid(newsquaresPerRow) {
+  const toDelete = document.querySelectorAll(".sketchElement");
+  toDelete.forEach((element) => {
+    element.remove();
+  });
+  for (let i = 1; i <= newsquaresPerRow * newsquaresPerRow; i++) {
+    const mydiv = document.createElement("div");
+    mydiv.classList.add("sketchElement");
+    container.appendChild(mydiv);
+    let divWidth = 640 / newsquaresPerRow;
+    mydiv.style.width = `${divWidth}px`;
+    mydiv.style.height = `${divWidth}px`;
+    mydiv.textContent = `A`;
+  }
+}
+
+const myButton = document.querySelector(".submit");
+const myInput = document.querySelector(".input");
+
+myButton.addEventListener("click", (event) => {
+  let newsquaresPerRow = myInput.value;
+  if (!isNaN(newsquaresPerRow) && newsquaresPerRow !== "") {
+    console.log(newsquaresPerRow);
+    updateGrid(newsquaresPerRow);
+  } else {
+    alert("Input a Number Only!");
+  }
+});
+
+const mydivList = document.querySelectorAll(".sketchElement");
